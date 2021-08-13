8d52738285f60dfd25c5087c9c34cc231b1ab266
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-08-13 05:44:28 +0200

    Add files via upload

---
+++ korting.js
@@ -0,0 +1,17 @@
+var myAge = 24;
+
+if (myAge >= 18 && myAge <= 25) {
+
+    console.log("Je krijgt 50% korting!");
+
+}
+
+var firstName = "Peter";
+
+if (firstName = "Sarah" || firstName = "Bram") {
+
+    console.log("Je krijgt een gratis biertje!");
+
+} else {
+    console.log("Dat wordt 2,50, alstublieft!");
+}
